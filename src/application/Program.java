42b8a394ab55da247b86ff258ab5a5f0c5f4dd8a
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-16 19:55:43 -0300

    LinkedHashSet Included

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -9,9 +10,10 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("HashSet: Mais rapido, mas nao gararante ordenacao.");
 		Set<String> hSet = new HashSet<>();
+		hSet.add("Notebook");
 		hSet.add("TV");
 		hSet.add("Tablet");
-		hSet.add("Notebook");
+		
 		
 		System.out.println(hSet.contains("Notebook"));
 		for (String p : hSet) {
@@ -23,11 +25,26 @@ public class Program {
 		System.out.println("TreeSet: Pouco mais lento , mas garante a ordencao.");
 		Set<String> tSet = new TreeSet<>();
 		tSet.add("Tv");
-		tSet.add("Tablet");
 		tSet.add("Notebook");
+		tSet.add("Tablet");
+		
 		
 		System.out.println(tSet.contains("Notebook"));
 		for (String p : tSet) {
+			System.out.println(p);
+		}
+		
+		System.out.println();
+		
+		System.out.println("LinkedHashSet: Pouco mais lento , mas mantem a ordencao de insercao.");
+		Set<String> lSet = new LinkedHashSet<>();
+		lSet.add("Tv");
+		lSet.add("Notebook");
+		lSet.add("Tablet");
+		
+		
+		System.out.println(lSet.contains("Notebook"));
+		for (String p : lSet) {
 			System.out.println(p);
 		}
 	}
