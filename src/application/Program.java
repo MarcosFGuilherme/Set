a913e303e86dddc98a478874404215998720d533
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-16 19:50:28 -0300

    TreeSet Included

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,22 +2,34 @@ package application;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Set<String> set = new HashSet<>();
-		set.add("TV");
-		set.add("Tablet");
-		set.add("Notebook");
+		System.out.println("HashSet: Mais rapido, mas nao gararante ordenacao.");
+		Set<String> hSet = new HashSet<>();
+		hSet.add("TV");
+		hSet.add("Tablet");
+		hSet.add("Notebook");
 		
-		System.out.println(set.contains("Notebook"));
-		for (String p : set) {
+		System.out.println(hSet.contains("Notebook"));
+		for (String p : hSet) {
 			System.out.println(p);
 		}
 		
+		System.out.println();
 		
+		System.out.println("TreeSet: Pouco mais lento , mas garante a ordencao.");
+		Set<String> tSet = new TreeSet<>();
+		tSet.add("Tv");
+		tSet.add("Tablet");
+		tSet.add("Notebook");
+		
+		System.out.println(tSet.contains("Notebook"));
+		for (String p : tSet) {
+			System.out.println(p);
+		}
 	}
 
 }
